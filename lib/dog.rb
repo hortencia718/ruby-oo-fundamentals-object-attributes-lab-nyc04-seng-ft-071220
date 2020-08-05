5838cb29b59e8e21cec2b05c579ee390e04dcea2
hortencia c <hortenciaecisneros@gmail.com>
2020-08-05 13:06:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -17,4 +17,6 @@ class Dog
   
 end
 
-# 
+# define the name of the dog "name" -->@name
+# create the name of the dog and set it to instance variable def name=(name) ---> @name=name
+# do the same for breed !
